5f6cc056c94ac184c97e922424de11f104e2fab8
Ben Keever <benkeever@Bens-MacBook-Air.local>
2021-11-18 14:24:16 -0500

    latest

--- GaussianMixtureCompatible.py
+++ GaussianMixtureCompatible.py
@@ -11,9 +11,8 @@ os.environ["OMP_NUM_THREADS"]="2"
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-def gaussian_mixture(data, first, last):
+def gaussian_mixture(data, first, last, plot):
     X = np.array(data)
-
     # calculate BIC for 1,2,3 and 4 clusters
     BIC = []
     for i in range(1, 5):
@@ -35,8 +34,8 @@ def gaussian_mixture(data, first, last):
     pos = np.array([X.ravel(), Y.ravel()]).T
     Z = np.exp(gmm.score_samples(pos))
     Z = Z.reshape(X.shape)
-
-    plot_heat_map(X, Y, Z, data, first, last)
+    if plot: plot_heat_map(X, Y, Z, data, first, last)
+    return gmm
 
 def plot_heat_map(X, Y, Z, data, first, last):
     #form contour plot
@@ -51,4 +50,21 @@ def plot_heat_map(X, Y, Z, data, first, last):
     else: plt.title(first + " " + last + "'s Probability Density Estimate")
     plt.show()
 
+def similarity_score(df1, df2, first1, last1, first2, last2):
+    data1, data2 = df1, df2
+    frames = [data1, data2]
+    data = pd.concat(frames)
 
+    gmm1 = gaussian_mixture(df1, first1, last1, False)
+    gmm2 = gaussian_mixture(df2, first2, last2, False)
+
+    X, Y = np.mgrid[data['Exit Velocity'].min() - 10:data['Exit Velocity'].max() + 10:1,
+           data['Launch Angle'].min() - 10:data['Launch Angle'].max() + 10:1]
+
+    # evaluate density at each point
+    pos = np.array([X.ravel(), Y.ravel()]).T
+    Z1 = np.exp(gmm1.score_samples(pos))
+    Z2 = np.exp(gmm2.score_samples(pos))
+    Z = Z1 - Z2
+    similarity_score = np.sum(np.abs(Z))
+    return similarity_score
--- PlayerSearch.py
+++ PlayerSearch.py
@@ -13,6 +13,8 @@ root.title("Gaussian Mixture Model For Batted Balls")
 
 instruction = Label(root, text="Player Search")
 instruction.grid(row = 0, column = 1, pady = 10)
+instruction2 = Label(root, text="Similarity Score")
+instruction2.grid(row = 4, column = 1, pady = 10)
 
 
 entries = []
@@ -21,14 +23,33 @@ for i in range(4):
     entry.grid(row = 2, column = i, padx= 0, pady= 0)
     entries.append(entry)
 
+
+for i in range(4):
+    entry = Entry(root)
+    entry.grid(row = 5, column = i, padx= 0, pady= 0)
+    entries.append(entry)
+
+for i in range(4):
+    entry = Entry(root)
+    entry.grid(row = 7, column = i, padx= 0, pady= 0)
+    entries.append(entry)
+
 firstname = Label(root, text="First")
 lastname = Label(root, text="Last")
 fromdate = Label(root, text="From")
 todate = Label(root, text="To")
-firstname.grid(row=3, column = 0, pady=1, padx = 1)
-lastname.grid(row=3, column = 1, pady=1, padx = 1)
-fromdate.grid(row=3, column = 2, pady=1, padx = 1)
-todate.grid(row=3, column = 3, pady=1, padx = 1)
+firstname.grid(row=1, column = 0, pady=1, padx = 1)
+lastname.grid(row=1, column = 1, pady=1, padx = 1)
+fromdate.grid(row=1, column = 2, pady=1, padx = 1)
+todate.grid(row=1, column = 3, pady=1, padx = 1)
+firstname = Label(root, text="First")
+lastname = Label(root, text="Last")
+fromdate = Label(root, text="From")
+todate = Label(root, text="To")
+firstname.grid(row=6, column = 0, pady=1, padx = 1)
+lastname.grid(row=6, column = 1, pady=1, padx = 1)
+fromdate.grid(row=6, column = 2, pady=1, padx = 1)
+todate.grid(row=6, column = 3, pady=1, padx = 1)
 
 
 def gaussian_mixture_model():
@@ -46,12 +67,44 @@ def gaussian_mixture_model():
     df = df.dropna(how='any')
     df = df.reindex(columns=["Exit Velocity","Launch Angle"])
 
-    GaussianMixtureCompatible.gaussian_mixture(df, first, last)
+    GaussianMixtureCompatible.gaussian_mixture(df, first, last, True)
+
+def similarity_score():
+    first1 = entries[4].get()
+    last1 = entries[5].get()
+    fromdate = entries[6].get()
+    todate = entries[7].get()
+    idDf = pd.DataFrame(playerid_lookup(last1, first1))
+    id = idDf["key_mlbam"][0]
+    playerstats = statcast_batter(fromdate, todate, id)
+    launchangle = playerstats["launch_angle"]
+    exitvelocity = playerstats["launch_speed"]
+    d = {"Launch Angle": launchangle, "Exit Velocity": exitvelocity}
+    df1 = pd.DataFrame(data=d)
+    df1 = df1.dropna(how='any')
+    df1 = df1.reindex(columns=["Exit Velocity", "Launch Angle"])
+
+    first2 = entries[8].get()
+    last2 = entries[9].get()
+    fromdate = entries[10].get()
+    todate = entries[11].get()
+    idDf = pd.DataFrame(playerid_lookup(last2, first2))
+    id = idDf["key_mlbam"][0]
+    playerstats = statcast_batter(fromdate, todate, id)
+    launchangle = playerstats["launch_angle"]
+    exitvelocity = playerstats["launch_speed"]
+    d = {"Launch Angle": launchangle, "Exit Velocity": exitvelocity}
+    df2 = pd.DataFrame(data=d)
+    df2 = df2.dropna(how='any')
+    df2 = df2.reindex(columns=["Exit Velocity", "Launch Angle"])
+
+    print(GaussianMixtureCompatible.similarity_score(df1, df2, first1, last1, first2, last2))
 
 
+gaussian_mixture_button = Button(root, text="Gaussian Mixture", command=gaussian_mixture_model)
+gaussian_mixture_button.grid(row = 3, column = 1, pady = 10)
 
-
-button = Button(root, text="Gaussian Mixture", command=gaussian_mixture_model)
-button.grid(row = 4, column = 1, pady = 10)
+similarity_score_button = Button(root, text="Similarity Score", command=similarity_score)
+similarity_score_button.grid(row = 8, column = 1, pady = 10)
 
 root.mainloop()
